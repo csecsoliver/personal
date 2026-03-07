d4b1f56ce9b77edbefcf100c9e2901a1cd83f49f
csecsoliver <0oliogamer0@gmail.com>
2026-03-07 21:23:34 +0100

    local counter init fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const counterEl = document.getElementById("counter")
 const messageEl = document.getElementById("message")
 const boardEl = document.getElementById("board")
-counterEl.innerText = parseInt(localStorage.getItem("counter"))
+counterEl.innerText = parseInt(localStorage.getItem("counter") ?? "0")
 document.getElementById("submit_message").addEventListener("click", async () => {
   counterEl.innerText = parseInt(localStorage.getItem("counter")) + 1
   localStorage.setItem("counter", parseInt(counterEl.innerText))
